96c4f8f686d452d8d669138128e280dde50f2a33
Safihre <safihre@sabnzbd.org>
2016-09-29 15:13:21 +0200

    Revert "dwedwed"
    This reverts commit 321ea7cc6c6832a9ddecfed0477b38558c8f9681.

--- runtest.py
+++ runtest.py
@@ -8,10 +8,7 @@ print
 data_raw = open("logo.ync", "rb").read()
 #data_raw = open("logo.ync", "rb").read()
 #sabyenc.decode_usenet_chunks('=ybegin part=41 line=128 size=49152000 name=90E2Sdvsmds0801dvsmds90E.part06.rar')
-
-test_list = ['abc', 'defg']
-
-output_buffer, output_filename, crc, crc_yenc, crc_correct = sabyenc.decode_usenet_chunks(data_raw, test_list)
+output_buffer, output_filename, crc, crc_yenc, crc_correct = sabyenc.decode_usenet_chunks(data_raw)
 
 print output_filename
 print crc
