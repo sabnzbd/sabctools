ddcb5fe81dd25769bf946ddf7e72d93e5e22d68b
Safihre <safihre@sabnzbd.org>
2017-02-27 10:08:36 +0100

    Test manylinux1

---
+++ travis/build-wheels.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e -x
+
+# Install a system package required by our libraries
+yum install -y python-devel
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
