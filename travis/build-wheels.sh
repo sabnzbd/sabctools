c449c6da622ed03fb6ac93c51c1211804e44d736
Safihre <safihre@sabnzbd.org>
2017-02-27 10:57:17 +0100

    Push to PyPi

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,10 +4,16 @@ set -e -x
 # Install a system package required by our libraries
 yum install -y python-devel
 
-# Compile wheels
-for PYBIN in /opt/python/*/bin; do
+# We only care about Python 2.7
+for PYBIN in /opt/python/cp27*/bin; do
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
+
+
+# Compile wheels for all platforms
+#for PYBIN in /opt/python/*/bin; do
+#    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+#done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
