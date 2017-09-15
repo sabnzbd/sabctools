9e674bcbbd0a0ea267df3a945d34b16294345699
Safihre <safihre@sabnzbd.org>
2017-09-15 12:54:45 +0200

    Bump to 3.1.1 to correct issues with sdist on PyPi

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import setup, Extension
 
 setup(
     name            = "sabyenc",
-    version         = "3.1.0",
+    version         = "3.1.1",
     author          = "Safihre",
     author_email    = "safihre@sabnzbd.org",
     url             = "https://github.com/sabnzbd/sabnzbd-yenc",
