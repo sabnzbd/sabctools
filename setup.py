0887381584c8de7687a9c31e3d3e98fd77c98b34
animetosho <animetosho@users.noreply.github.com>
2022-02-22 12:03:18 +1000

    Integrate build steps into build_ext

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@
 #
 
 from setuptools import setup, Extension
-from distutils import ccompiler
+from setuptools.command.build_ext import build_ext
 import sysconfig
 import platform
 
@@ -30,63 +30,78 @@ with open("README.md", "r") as file_long_description:
     long_description = file_long_description.read()
 
 BUILD_DIR = "build"
-compiler = ccompiler.new_compiler()
-if compiler.compiler_type == "msvc":
-    # LTCG not enabled due to issues seen with code generation where different ISA extensions are selected for specific files
-    base_ldflags = ["/OPT:REF", "/OPT:ICF"]
-    cflags = ["/O2", "/GS-", "/Gy", "/sdl-", "/Oy", "/Oi"]
-else:
-    # TODO: consider -flto - may require some extra testing
-    base_ldflags = sysconfig.get_config_var('LDFLAGS').split()
-    cflags = ["-Wall", "-Wextra", "-Wno-unused-function", "-fomit-frame-pointer", "-fno-rtti", "-fno-exceptions", "-O3", "-fPIC"] + sysconfig.get_config_var('CFLAGS').split()
-
 machine = platform.machine().lower()  # TODO: this is unreliable, need something better to detect target architecture
 is_armv7 = (machine.startswith("armv7"))  # TODO: can armv8_ still be AArch32?
 is_arm = (is_armv7 or machine.startswith("arm") or machine.startswith("aarch64"))
 is_x86 = (machine in ["i386", "i686", "x86", "x86_64", "x64", "amd64"])
-objects = []
-# TODO: move this to compile step so setup.py --help doesn't trigger compilation
-# TODO: add depends
-for obj in [
-    {"sources": ["yencode/encoder_sse2.cc", "yencode/decoder_sse2.cc"], "gcc_x86_flags": ["-msse2"]},
-    {"sources": ["yencode/encoder_ssse3.cc", "yencode/decoder_ssse3.cc"], "gcc_x86_flags": ["-mssse3"]},
-    {"sources": ["yencode/crc_folding.cc"], "gcc_x86_flags": ["-mssse3", "-msse4.1", "-mpclmul"]},
-    {"sources": ["yencode/encoder_avx.cc", "yencode/decoder_avx.cc"], "gcc_x86_flags": ["-mavx", "-mpopcnt"], "msvc_x86_flags": ["/arch:AVX"]},
-    {"sources": ["yencode/encoder_avx2.cc", "yencode/decoder_avx2.cc"], "gcc_x86_flags": ["-mavx2", "-mpopcnt", "-mbmi", "-mbmi2", "-mlzcnt"], "msvc_x86_flags": ["/arch:AVX2"]},
-    {"sources": ["yencode/encoder_neon.cc", ("yencode/decoder_neon.cc" if is_armv7 else "yencode/decoder_neon64.cc")], "gcc_arm_flags": (["-mfpu=neon"] if is_armv7 else [])},
-    {"sources": ["yencode/crc_arm.cc"], "gcc_arm_flags": ["-march=armv8-a+crc"]},
-    {"sources": [
-        "crcutil-1.0/code/crc32c_sse4.cc",
-        "crcutil-1.0/code/multiword_64_64_cl_i386_mmx.cc",
-        "crcutil-1.0/code/multiword_64_64_gcc_amd64_asm.cc",
-        "crcutil-1.0/code/multiword_64_64_gcc_i386_mmx.cc",
-        "crcutil-1.0/code/multiword_64_64_intrinsic_i386_mmx.cc",
-        "crcutil-1.0/code/multiword_128_64_gcc_amd64_sse2.cc",
-        "crcutil-1.0/examples/interface.cc"
-    ], "gcc_flags": ["-Wno-expansion-to-defined"], "include_dirs": ["crcutil-1.0/code", "crcutil-1.0/tests"], "macros": [("CRCUTIL_USE_MM_CRC32", "0")]}
-]:
-    args = {
-        "sources": obj["sources"],
-        "output_dir": BUILD_DIR
-    }
-    args["extra_postargs"] = cflags[:]
-    if compiler.compiler_type == "msvc":
-        if is_x86 and "msvc_x86_flags" in obj:
-            args["extra_postargs"] += obj["msvc_x86_flags"]
-    else:
-        if "gcc_flags" in obj:
-            args["extra_postargs"] += obj["gcc_flags"]
-        if is_x86 and "gcc_x86_flags" in obj:
-            args["extra_postargs"] += obj["gcc_x86_flags"]
-        if is_arm and "gcc_arm_flags" in obj:
-            args["extra_postargs"] += obj["gcc_arm_flags"]
-    
-    if "include_dirs" in obj:
-        args["include_dirs"] = obj["include_dirs"]
-    if "macros" in obj:
-        args["macros"] = obj["macros"]
-    compiler.compile(**args)
-    objects += compiler.object_filenames(obj["sources"], output_dir=BUILD_DIR)
+
+
+
+class sabyenc_build(build_ext):
+    def build_extension(self, ext):
+        # determine compiler flags
+        compiler = self.compiler
+        if compiler.compiler_type == "msvc":
+            # LTCG not enabled due to issues seen with code generation where different ISA extensions are selected for specific files
+            base_ldflags = ["/OPT:REF", "/OPT:ICF"]
+            cflags = ["/O2", "/GS-", "/Gy", "/sdl-", "/Oy", "/Oi"]
+        else:
+            # TODO: consider -flto - may require some extra testing
+            base_ldflags = sysconfig.get_config_var('LDFLAGS').split()
+            cflags = ["-Wall", "-Wextra", "-Wno-unused-function", "-fomit-frame-pointer", "-fno-rtti", "-fno-exceptions", "-O3", "-fPIC"] + sysconfig.get_config_var('CFLAGS').split()
+        
+        
+        # build yencode/crcutil
+        objects = []
+        # TODO: add depends
+        for obj in [
+            {"sources": ["yencode/encoder_sse2.cc", "yencode/decoder_sse2.cc"], "gcc_x86_flags": ["-msse2"]},
+            {"sources": ["yencode/encoder_ssse3.cc", "yencode/decoder_ssse3.cc"], "gcc_x86_flags": ["-mssse3"]},
+            {"sources": ["yencode/crc_folding.cc"], "gcc_x86_flags": ["-mssse3", "-msse4.1", "-mpclmul"]},
+            {"sources": ["yencode/encoder_avx.cc", "yencode/decoder_avx.cc"], "gcc_x86_flags": ["-mavx", "-mpopcnt"], "msvc_x86_flags": ["/arch:AVX"]},
+            {"sources": ["yencode/encoder_avx2.cc", "yencode/decoder_avx2.cc"], "gcc_x86_flags": ["-mavx2", "-mpopcnt", "-mbmi", "-mbmi2", "-mlzcnt"], "msvc_x86_flags": ["/arch:AVX2"]},
+            {"sources": ["yencode/encoder_neon.cc", ("yencode/decoder_neon.cc" if is_armv7 else "yencode/decoder_neon64.cc")], "gcc_arm_flags": (["-mfpu=neon"] if is_armv7 else [])},
+            {"sources": ["yencode/crc_arm.cc"], "gcc_arm_flags": ["-march=armv8-a+crc"]},
+            {"sources": [
+                "crcutil-1.0/code/crc32c_sse4.cc",
+                "crcutil-1.0/code/multiword_64_64_cl_i386_mmx.cc",
+                "crcutil-1.0/code/multiword_64_64_gcc_amd64_asm.cc",
+                "crcutil-1.0/code/multiword_64_64_gcc_i386_mmx.cc",
+                "crcutil-1.0/code/multiword_64_64_intrinsic_i386_mmx.cc",
+                "crcutil-1.0/code/multiword_128_64_gcc_amd64_sse2.cc",
+                "crcutil-1.0/examples/interface.cc"
+            ], "gcc_flags": ["-Wno-expansion-to-defined"], "include_dirs": ["crcutil-1.0/code", "crcutil-1.0/tests"], "macros": [("CRCUTIL_USE_MM_CRC32", "0")]}
+        ]:
+            args = {
+                "sources": obj["sources"],
+                "output_dir": BUILD_DIR
+            }
+            args["extra_postargs"] = cflags[:]
+            if compiler.compiler_type == "msvc":
+                if is_x86 and "msvc_x86_flags" in obj:
+                    args["extra_postargs"] += obj["msvc_x86_flags"]
+            else:
+                if "gcc_flags" in obj:
+                    args["extra_postargs"] += obj["gcc_flags"]
+                if is_x86 and "gcc_x86_flags" in obj:
+                    args["extra_postargs"] += obj["gcc_x86_flags"]
+                if is_arm and "gcc_arm_flags" in obj:
+                    args["extra_postargs"] += obj["gcc_arm_flags"]
+            
+            if "include_dirs" in obj:
+                args["include_dirs"] = obj["include_dirs"]
+            if "macros" in obj:
+                args["macros"] = obj["macros"]
+            compiler.compile(**args)
+            objects += compiler.object_filenames(obj["sources"], output_dir=BUILD_DIR)
+        
+        # attach to Extension
+        ext.extra_compile_args = cflags
+        ext.extra_link_args = base_ldflags + objects
+        ext.depends = ["src/sabyenc3.h"] + objects
+        
+        # proceed with regular Extension build
+        super(sabyenc_build, self).build_extension(ext)
 
 setup(
     name="sabyenc3",
@@ -99,11 +114,9 @@ setup(
     ext_modules=[Extension(
         "sabyenc3",
         ["src/sabyenc3.c", "yencode/platform.cc", "yencode/encoder.cc", "yencode/decoder.cc", "yencode/crc.cc"],
-        extra_compile_args=cflags,
-        extra_link_args=base_ldflags + objects,
-        depends=["src/sabyenc3.h"] + objects,
         include_dirs=["crcutil-1.0/code","crcutil-1.0/examples"]
     )],
+    cmdclass = {"build_ext": sabyenc_build},
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
