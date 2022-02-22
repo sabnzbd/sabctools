5d17f21924b402eb196e9a8f7ef3753d61d0bb82
animetosho <animetosho@users.noreply.github.com>
2022-02-22 12:19:08 +1000

    Add yencode source dependencies

--- setup.py
+++ setup.py
@@ -51,26 +51,79 @@ class sabyenc_build(build_ext):
             cflags = ["-Wall", "-Wextra", "-Wno-unused-function", "-fomit-frame-pointer", "-fno-rtti", "-fno-exceptions", "-O3", "-fPIC"] + sysconfig.get_config_var('CFLAGS').split()
         
         
+        srcdeps_crc_common = ["yencode/common.h", "yencode/crc_common.h", "yencode/crc.h"]
+        srcdeps_dec_common = ["yencode/common.h", "yencode/decoder_common.h", "yencode/decoder.h"]
+        srcdeps_enc_common = ["yencode/common.h", "yencode/encoder_common.h", "yencode/encoder.h"]
         # build yencode/crcutil
+        # TODO: consider multi-threading + only compile modified sources
         objects = []
-        # TODO: add depends
         for obj in [
-            {"sources": ["yencode/encoder_sse2.cc", "yencode/decoder_sse2.cc"], "gcc_x86_flags": ["-msse2"]},
-            {"sources": ["yencode/encoder_ssse3.cc", "yencode/decoder_ssse3.cc"], "gcc_x86_flags": ["-mssse3"]},
-            {"sources": ["yencode/crc_folding.cc"], "gcc_x86_flags": ["-mssse3", "-msse4.1", "-mpclmul"]},
-            {"sources": ["yencode/encoder_avx.cc", "yencode/decoder_avx.cc"], "gcc_x86_flags": ["-mavx", "-mpopcnt"], "msvc_x86_flags": ["/arch:AVX"]},
-            {"sources": ["yencode/encoder_avx2.cc", "yencode/decoder_avx2.cc"], "gcc_x86_flags": ["-mavx2", "-mpopcnt", "-mbmi", "-mbmi2", "-mlzcnt"], "msvc_x86_flags": ["/arch:AVX2"]},
-            {"sources": ["yencode/encoder_neon.cc", ("yencode/decoder_neon.cc" if is_armv7 else "yencode/decoder_neon64.cc")], "gcc_arm_flags": (["-mfpu=neon"] if is_armv7 else [])},
-            {"sources": ["yencode/crc_arm.cc"], "gcc_arm_flags": ["-march=armv8-a+crc"]},
-            {"sources": [
-                "crcutil-1.0/code/crc32c_sse4.cc",
-                "crcutil-1.0/code/multiword_64_64_cl_i386_mmx.cc",
-                "crcutil-1.0/code/multiword_64_64_gcc_amd64_asm.cc",
-                "crcutil-1.0/code/multiword_64_64_gcc_i386_mmx.cc",
-                "crcutil-1.0/code/multiword_64_64_intrinsic_i386_mmx.cc",
-                "crcutil-1.0/code/multiword_128_64_gcc_amd64_sse2.cc",
-                "crcutil-1.0/examples/interface.cc"
-            ], "gcc_flags": ["-Wno-expansion-to-defined"], "include_dirs": ["crcutil-1.0/code", "crcutil-1.0/tests"], "macros": [("CRCUTIL_USE_MM_CRC32", "0")]}
+            {
+                "sources": ["yencode/encoder_sse2.cc"],
+                "depends": srcdeps_enc_common + ["encoder_sse_base.h"],
+                "gcc_x86_flags": ["-msse2"]
+            }, {
+                "sources": ["yencode/decoder_sse2.cc"],
+                "depends": srcdeps_dec_common + ["decoder_sse_base.h"],
+                "gcc_x86_flags": ["-msse2"]
+            }, {
+                "sources": ["yencode/encoder_ssse3.cc"],
+                "depends": srcdeps_enc_common + ["encoder_sse_base.h"],
+                "gcc_x86_flags": ["-mssse3"]
+            }, {
+                "sources": ["yencode/decoder_ssse3.cc"],
+                "depends": srcdeps_dec_common + ["decoder_sse_base.h"],
+                "gcc_x86_flags": ["-mssse3"]
+            }, {
+                "sources": ["yencode/crc_folding.cc"],
+                "depends": srcdeps_crc_common,
+                "gcc_x86_flags": ["-mssse3", "-msse4.1", "-mpclmul"]
+            }, {
+                "sources": ["yencode/encoder_avx.cc"],
+                "depends": srcdeps_enc_common + ["encoder_sse_base.h"],
+                "gcc_x86_flags": ["-mavx", "-mpopcnt"],
+                "msvc_x86_flags": ["/arch:AVX"]
+            }, {
+                "sources": ["yencode/decoder_avx.cc"],
+                "depends": srcdeps_dec_common + ["decoder_sse_base.h"],
+                "gcc_x86_flags": ["-mavx", "-mpopcnt"],
+                "msvc_x86_flags": ["/arch:AVX"]
+            }, {
+                "sources": ["yencode/encoder_avx2.cc"],
+                "depends": srcdeps_enc_common + ["encoder_avx_base.h"],
+                "gcc_x86_flags": ["-mavx2", "-mpopcnt", "-mbmi", "-mbmi2", "-mlzcnt"],
+                "msvc_x86_flags": ["/arch:AVX2"]
+            }, {
+                "sources": ["yencode/decoder_avx2.cc"],
+                "depends": srcdeps_dec_common + ["decoder_avx2_base.h"],
+                "gcc_x86_flags": ["-mavx2", "-mpopcnt", "-mbmi", "-mbmi2", "-mlzcnt"],
+                "msvc_x86_flags": ["/arch:AVX2"]
+            }, {
+                "sources": ["yencode/encoder_neon.cc"],
+                "depends": srcdeps_enc_common,
+                "gcc_arm_flags": (["-mfpu=neon"] if is_armv7 else [])
+            }, {
+                "sources": ["yencode/decoder_neon.cc" if is_armv7 else "yencode/decoder_neon64.cc"],
+                "depends": srcdeps_dec_common,
+                "gcc_arm_flags": (["-mfpu=neon"] if is_armv7 else [])
+            }, {
+                "sources": ["yencode/crc_arm.cc"],
+                "depends": srcdeps_crc_common,
+                "gcc_arm_flags": ["-march=armv8-a+crc"]
+            }, {
+                "sources": [
+                    "crcutil-1.0/code/crc32c_sse4.cc",
+                    "crcutil-1.0/code/multiword_64_64_cl_i386_mmx.cc",
+                    "crcutil-1.0/code/multiword_64_64_gcc_amd64_asm.cc",
+                    "crcutil-1.0/code/multiword_64_64_gcc_i386_mmx.cc",
+                    "crcutil-1.0/code/multiword_64_64_intrinsic_i386_mmx.cc",
+                    "crcutil-1.0/code/multiword_128_64_gcc_amd64_sse2.cc",
+                    "crcutil-1.0/examples/interface.cc"
+                ],
+                "gcc_flags": ["-Wno-expansion-to-defined"],
+                "include_dirs": ["crcutil-1.0/code", "crcutil-1.0/tests"],
+                "macros": [("CRCUTIL_USE_MM_CRC32", "0")]
+            }
         ]:
             args = {
                 "sources": obj["sources"],
