5b2d71649c5b389712919fd00e53c589560439e5
Safihre <safihre@sabnzbd.org>
2022-03-23 09:31:20 +0100

    Remove Python 2 code in speed_compare
    Closes #34

--- tests/speed_compare.py
+++ tests/speed_compare.py
@@ -16,24 +16,17 @@ with open("tests/yencfiles/test_regular.txt", "rb") as yencfile:
     n = 2**chunk_size
     data_chunks = [data_raw[i : i + n] for i in range(0, len(data_raw), n)]
 
-try:
-    # Python 2
-    get_time = time.clock
-except AttributeError:
-    # Python 3
-    get_time = time.process_time
-
 ###################
 # YENC C - NEW
 ###################
 
 # Time it!
-time1_new = get_time()
+time1_new = time.process_time()
 
 for i in range(nr_runs):
     decoded_data_new, output_filename, crc_correct = sabyenc3.decode_usenet_chunks(data_chunks)
 
 print("---")
-time1_new_disp = 1000 * (get_time() - time1_new)
+time1_new_disp = 1000 * (time.process_time() - time1_new)
 print("%15s  took  %4d ms" % ("yEnc C New", time1_new_disp))
 print("---")
