ca957fdfebd2adc31b66a8b648dd3034de51b8df
Coda <822628+mayli@users.noreply.github.com>
2022-10-09 15:01:51 -0700

    Update test_encoder.py

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -5,3 +5,8 @@ def test_encoder():
     output, crc = sabyenc3.encode(b"Hello world!")
     assert output == b"r\x8f\x96\x96\x99J\xa1\x99\x9c\x96\x8eK"
     assert crc == 0x1B851995
+
+def test_encoder_memoryview():
+    output, crc = sabyenc3.encode(memoryview(b"Hello world!"))
+    assert output == b"r\x8f\x96\x96\x99J\xa1\x99\x9c\x96\x8eK"
+    assert crc == 0x1B851995
