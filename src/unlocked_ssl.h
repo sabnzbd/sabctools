705ca783fb111e5989783c4076c7f5ec735d6ed3
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2023-01-09 08:49:24 +0000

    Remove unused includes and macros

--- src/unlocked_ssl.h
+++ src/unlocked_ssl.h
@@ -11,6 +11,7 @@
 # define SABYENC_DLL_CALL __stdcall
 # define WIN32_LEAN_AND_MEAN
 # include <Windows.h>
+# include <winsock2.h>
 #else
 # define SABYENC_DLL_CALL
 # include <dlfcn.h>
@@ -25,38 +26,6 @@ extern "C" {
 # define SSL_ERROR_WANT_READ 2
 # define SSL_ERROR_WANT_WRITE 3
 # define SSL_ERROR_ZERO_RETURN 6
-
-#ifndef MS_WINDOWS
-#ifdef __VMS
-#   include <socket.h>
-# else
-#   include <sys/socket.h>
-# endif
-# include <netinet/in.h>
-# include <netinet/tcp.h>
-
-#else /* MS_WINDOWS */
-# include <winsock2.h>
-
-#ifdef MS_WINDOWS
-#  define WIN32_LEAN_AND_MEAN
-#  include <winsock.h>
-#else
-#  define SOCKET int
-#endif
-#endif
-
-#ifdef MS_WINDOWS
-typedef SOCKET SOCKET_T;
-#       ifdef MS_WIN64
-#               define SIZEOF_SOCKET_T 8
-#       else
-#               define SIZEOF_SOCKET_T 4
-#       endif
-#else
-typedef int SOCKET_T;
-#       define SIZEOF_SOCKET_T SIZEOF_INT
-#endif
 
 void openssl_init();
 bool openssl_linked();
