ff19846f3515c3e52677e19c30fd4829e4d1740b
Safihre <safihre@sabnzbd.org>
2023-02-18 22:08:57 +0100

    Restructure yencfiles used in tests

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -5,14 +5,14 @@ from tests.testsupport import *
 
 
 def test_regular():
-    data_plain = read_plain_yenc_file("test_regular.txt")
+    data_plain = read_plain_yenc_file("test_regular.yenc")
     assert python_yenc(data_plain) == sabctools_yenc_wrapper(data_plain)
-    data_plain = read_plain_yenc_file("test_regular_2.txt")
+    data_plain = read_plain_yenc_file("test_regular_2.yenc")
     assert python_yenc(data_plain) == sabctools_yenc_wrapper(data_plain)
 
 
 def test_partial():
-    data_plain = read_plain_yenc_file("test_partial.txt")
+    data_plain = read_plain_yenc_file("test_partial.yenc")
     decoded_data, filename, crc_correct = sabctools_yenc_wrapper(data_plain)
     assert filename == "90E2Sdvsmds0801dvsmds90E.part06.rar"
     assert crc_correct is None
@@ -20,39 +20,39 @@ def test_partial():
 
 
 def test_special_chars():
-    data_plain = read_plain_yenc_file("test_special_chars.txt")
+    data_plain = read_plain_yenc_file("test_special_chars.yenc")
     # We only compare the data and the filename
     assert python_yenc(data_plain) == sabctools_yenc_wrapper(data_plain)
 
     data_plain = bytearray(
-        b"=ybegin part=1 total=1 line=128 size=6 name=Hi Kingdom \xe4\xbd\xa0\xe5\xa5\xbd\xe4\xb8\x96\xe7\x95\x8c.txt\r\n=ypart begin=1 end=6\r\nr\x8f\x96\x96\x994\r\n=yend size=6 part=1 pcrc32=31963516 crc32=31963516\r\n"
+        b"=ybegin part=1 total=1 line=128 size=6 name=Hi Kingdom \xe4\xbd\xa0\xe5\xa5\xbd\xe4\xb8\x96\xe7\x95\x8c.yenc\r\n=ypart begin=1 end=6\r\nr\x8f\x96\x96\x994\r\n=yend size=6 part=1 pcrc32=31963516 crc32=31963516\r\n"
     )
     # We only compare the data and the filename
     assert python_yenc(data_plain) == sabctools_yenc_wrapper(data_plain)
 
 
 def test_bad_crc():
-    data_plain = read_plain_yenc_file("test_badcrc.txt")
+    data_plain = read_plain_yenc_file("test_bad_crc.yenc")
     # We only compare the data and the filename
     assert python_yenc(data_plain) == sabctools_yenc_wrapper(data_plain)
 
 
 def test_bad_crc_end():
-    data_plain = read_plain_yenc_file("test_bad_crc_end.txt")
+    data_plain = read_plain_yenc_file("test_bad_crc_end.yenc")
     with pytest.raises(ValueError) as excinfo:
         sabctools_yenc_wrapper(data_plain)
     assert "Invalid CRC in footer" in str(excinfo.value)
 
 
 def test_no_filename():
-    data_plain = read_plain_yenc_file("test_no_name.txt")
+    data_plain = read_plain_yenc_file("test_no_name.yenc")
     with pytest.raises(ValueError) as excinfo:
         sabctools_yenc_wrapper(data_plain)
     assert "Could not find yEnc filename" in str(excinfo.value)
 
 
 def test_end_after_filename():
-    data_plain = read_plain_yenc_file("test_end_after_filename.txt")
+    data_plain = read_plain_yenc_file("test_end_after_filename.yenc")
     with pytest.raises(ValueError):
         sabctools_yenc_wrapper(data_plain)
 
@@ -66,7 +66,7 @@ def test_bad_size():
 def test_ref_counts():
     """Note that sys.getrefcount itself adds another reference!"""
     # Test regular case
-    data_plain = read_plain_yenc_file("test_regular.txt")
+    data_plain = read_plain_yenc_file("test_regular.yenc")
     data_out, filename, crc_correct = sabctools_yenc_wrapper(data_plain)
     # data_plain and data_out point to the same data!
     assert sys.getrefcount(data_plain) == 3
@@ -82,7 +82,7 @@ def test_ref_counts():
     assert sys.getrefcount(fake_inp) == 2
 
     # Test further processing
-    data_plain = read_plain_yenc_file("test_bad_crc_end.txt")
+    data_plain = read_plain_yenc_file("test_bad_crc_end.yenc")
     with pytest.raises(ValueError):
         sabctools_yenc_wrapper(data_plain)
     assert sys.getrefcount(data_plain) == 2
@@ -90,7 +90,7 @@ def test_ref_counts():
 
 def test_bad_filename_pickle():
     # This one fails in the old yEnc in different way
-    data_plain = read_pickle("tests/yencfiles/split_filename")
+    data_plain = read_pickle("tests/yencfiles/split_filename.pickle")
     decoded_data, filename, crc_correct = sabctools_yenc_wrapper(data_plain)
     assert filename == "Low.Winter.Sun.US.S01E01.720p.BluRay.x264-DEMAND.part04.rar"
     assert crc_correct is None
@@ -100,13 +100,6 @@ def test_bad_filename_pickle():
 def test_crc_pickles():
     all_crc_fails = glob.glob("tests/yencfiles/crc_*")
     for fname in all_crc_fails:
-        data_plain = read_pickle(fname)
-        assert python_yenc(data_plain) == sabctools_yenc_wrapper(data_plain)
-
-
-def test_huge_file_pickles():
-    all_pickles = glob.glob("tests/yencfiles/huge_file*")
-    for fname in all_pickles:
         data_plain = read_pickle(fname)
         assert python_yenc(data_plain) == sabctools_yenc_wrapper(data_plain)
 
