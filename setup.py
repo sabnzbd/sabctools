7565afdb3a1e8ca9ec22a1af9251cf02773e3722
animetosho <animetosho@users.noreply.github.com>
2024-05-18 23:02:28 +1000

    Remove UNSUPPORTED_PLATFORM_ARM Ref #114

--- setup.py
+++ setup.py
@@ -124,13 +124,6 @@ class SABCToolsBuild(build_ext):
             # Verify specific flags for ARM chips
             # macOS M1 do not need any flags, they support everything
             if IS_ARM and not IS_MACOS:
-                if not autoconf_check(self.compiler, include_check="sys/auxv.h"):
-                    # We only tested this for GCC, might still be valid on MS-ARM compiler (see #38)
-                    if autoconf_check(self.compiler, define_check="__GNUC__"):
-                        log.info("==> On GGC and sys/auxv.h not available, setting UNSUPPORTED_PLATFORM_ARM=1")
-                        ext.define_macros.append(("UNSUPPORTED_PLATFORM_ARM", "1"))
-                        gcc_macros.append(("UNSUPPORTED_PLATFORM_ARM", "1"))
-                        IS_ARM = False
                 if not autoconf_check(self.compiler, define_check="__aarch64__"):
                     log.info("==> __aarch64__ not available, disabling 64bit extensions")
                     IS_AARCH64 = False
